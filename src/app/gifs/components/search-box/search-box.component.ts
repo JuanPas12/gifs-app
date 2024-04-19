0f2787571c788200659221521660e97df6de100d
Juan Pablo Tafolla <juanptafolla@gmail.com>
2024-04-19 13:43:54 -0600

    @ViewChild - Referencia al HTML

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  styleUrls: ['./search-box.component.css'],
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput') public tagInput!: ElementRef;
 
+  constructor() {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log(newTag);
+  }
 }
