a35313e9d45bc985db832db8b55dacee67c79840
Juan Pablo Tafolla <juanptafolla@gmail.com>
2024-04-22 10:24:04 -0600

    Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,6 +13,19 @@ export class GifsService {
   }
 
   searchTag(tag: string): void {
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this.tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((t) => t !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 }
